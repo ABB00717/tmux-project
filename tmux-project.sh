def18be02241e07d36e0683b70aa3b658066dd04
abb00717 <abb00717@gmail.com>
2025-08-27 15:16:47 +0800

    implement basic functions
    - create new session with the dir-name
    - create 3 window(code | terminal | docs) in the working-dir

---
+++ tmux-project.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+NEW_SESSION=$1
+
+# TODO: Simplify the name
+WORKING_DIR=$1
+
+# tmux open new session (detach immediately since the script isn't finish yet)
+tmux new -d -c $WORKING_DIR -s $NEW_SESSION
+
+# create 3 windows: code | terminal | docs
+tmux rename-window -c $WORKING_DIR -t $NEW_SESSION code
+tmux new-window -c $WORKING_DIR -t $NEW_SESSION -n terminal
+tmux new-window -c $WORKING_DIR -t $NEW_SESSION -n docs
+
+# Now you can attach the session
+tmux attach -t $NEW_SESSION
