f838b25dde3d5dc9a2cfbc2423b06bf0f34dce8e
abb00717 <abb00717@gmail.com>
2025-08-27 15:46:47 +0800

    fix an issue where the first window wasn't named correctly

--- tmux-project.sh
+++ tmux-project.sh
@@ -9,7 +9,7 @@ WORKING_DIR=$1
 tmux new -d -c $WORKING_DIR -s $NEW_SESSION
 
 # create 3 windows: code | terminal | docs
-tmux rename-window -c $WORKING_DIR -t $NEW_SESSION code
+tmux rename-window -t $NEW_SESSION code
 tmux new-window -c $WORKING_DIR -t $NEW_SESSION -n terminal
 tmux new-window -c $WORKING_DIR -t $NEW_SESSION -n docs
 
