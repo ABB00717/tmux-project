43b44574fdb1092bdc00ac4115a28bec70bf552b
abb00717 <abb00717@gmail.com>
2025-08-30 00:12:43 +0800

    add a new window called help

--- tmux-project.sh
+++ tmux-project.sh
@@ -12,10 +12,11 @@ NEW_SESSION=$(basename "$WORKING_DIR")
 # tmux open new session (detach immediately since the script isn't finish yet)
 tmux new -d -c $WORKING_DIR -s $NEW_SESSION
 
-# create 3 windows: code | terminal | docs
+# create 4 windows: code | terminal | docs | help
 tmux rename-window -t $NEW_SESSION code
 tmux new-window -c $WORKING_DIR -t $NEW_SESSION -n terminal
 tmux new-window -c $WORKING_DIR -t $NEW_SESSION -n docs
+tmux new-window -c $WORKING_DIR -t $NEW_SESSION -n help
 
 # Select the code window
 tmux select-window -t code
