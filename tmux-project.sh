e8b6bdda82fbe071730d7efcf667bb69b0750932
abb00717 <abb00717@gmail.com>
2025-10-07 03:47:21 +0800

    fix: replace all '.' in dir name with '-'

--- tmux-project.sh
+++ tmux-project.sh
@@ -7,19 +7,20 @@ fi
 
 # Simplify the session-name
 WORKING_DIR="$1"
-NEW_SESSION=$(basename "$WORKING_DIR")
+NEW_SESSION=$(basename "$WORKING_DIR" | sed 's/\./-/g')
 
 # tmux open new session (detach immediately since the script isn't finish yet)
-tmux new -d -c $WORKING_DIR -s $NEW_SESSION
+tmux new -d -c "$WORKING_DIR" -s "$NEW_SESSION"
 
-# create 4 windows: code | terminal | docs | help
-tmux rename-window -t $NEW_SESSION code
-tmux new-window -c $WORKING_DIR -t $NEW_SESSION -n terminal
-tmux new-window -c $WORKING_DIR -t $NEW_SESSION -n docs
-tmux new-window -c $WORKING_DIR -t $NEW_SESSION -n help
+# create 5 windows: code | terminal | docs | help | debugger
+tmux rename-window -t "$NEW_SESSION" code
+tmux new-window -c "$WORKING_DIR" -t "$NEW_SESSION" -n terminal
+tmux new-window -c "$WORKING_DIR" -t "$NEW_SESSION" -n docs
+tmux new-window -c "$WORKING_DIR" -t "$NEW_SESSION" -n help
+tmux new-window -c "$WORKING_DIR" -t "$NEW_SESSION" -n debugger
 
 # Select the code window
-tmux select-window -t code
+tmux select-window -t "$NEW_SESSION:code"
 
 # Now you can attach the session
-tmux attach -t $NEW_SESSION
+tmux attach -t "$NEW_SESSION"
